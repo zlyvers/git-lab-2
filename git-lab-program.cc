451e7797e8b6dcdadc7fd796f4e0fcac2c27b104
Zane <zlyvers943524@ohio.edu>
2025-01-21 11:23:06 -0500

    c++ code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+    File: git-lab-program.cc
+    Author: Zane Lyvers
+    Date: 01/21/2025
+    Description: The code for the git-lab
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
